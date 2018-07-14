6309452646938b7354aa1b2d8a6ffd6f5092e48c
asterixorobelix <nathanstasin@gmail.com>
2018-07-14 14:46:44 +0200

    added async events

--- Async/favBrowser/favBrowser/App.xaml.cs
+++ Async/favBrowser/favBrowser/App.xaml.cs
@@ -13,5 +13,17 @@ namespace favBrowser
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            var eventHandlerWindow = new EventHandlerWindow
+            {
+                Top = 100,
+
+                Left = 400
+            };
+
+            eventHandlerWindow.Show();
+        }
     }
 }
