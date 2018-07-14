c7ed6cc75113b8483f64d7dd20bd08e0fc14b31f
asterixorobelix <nathanstasin@gmail.com>
2018-07-14 10:51:35 +0200

    initial commit

---
+++ Async/faviconbrowser/FaviconBrowser/MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+
+namespace FaviconBrowser
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private static readonly List<string> s_Domains = new List<string>
+        {
+            "google.com",
+            "bing.com",
+            "oreilly.com",
+            "microsoft.com",
+            "twitter.com",
+            "reddit.com",
+            "baidu.com",
+            "bbc.co.uk",
+            "bitbucket.org",
+            "pinterest.com",
+            "fastcompany.com",
+            "behance.net",
+            "netflix.com",
+            "hackaday.com",
+            "monocle.com",
+            "getbootstrap.com",
+        };
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void GetButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            foreach (string domain in s_Domains)
+            {
+                AddAFavicon(domain);
+            }
+        }
+
+        private void AddAFavicon(string domain)
+        {
+            WebClient webClient = new WebClient();
+            byte[] bytes = webClient.DownloadData("http://" + domain + "/favicon.ico");
+            Image imageControl = MakeImageControl(bytes);
+            m_WrapPanel.Children.Add(imageControl);
+        }
+
+        private static Image MakeImageControl(byte[] bytes)
+        {
+            Image imageControl = new Image();
+            BitmapImage bitmapImage = new BitmapImage();
+            bitmapImage.BeginInit();
+            bitmapImage.StreamSource = new MemoryStream(bytes);
+            bitmapImage.EndInit();
+            imageControl.Source = bitmapImage;
+            imageControl.Width = 25;
+            imageControl.Height = 25;
+            return imageControl;
+        }
+    }
+}
